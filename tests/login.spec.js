d0026e0f5ce0083ebd8dd4715b09ebbee1b8b135
Paras Kalura <officialkalura@gmail.com>
2026-09-24 07:15:09 +0530

    Add login validation tests

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -9,3 +9,18 @@ test('Invalid Password', async ({page}) => {
  await expect(page.getByText("Password do not match")).toBeVisible();
 
 })
+
+test('Invalid Username', async ({page}) => {
+
+    await page.goto('https://www.saucedemo.com/');
+
+    await page.getByPlaceholder('Username').fill('wrong_user');
+    await page.getByPlaceholder('Password').fill('secret_sauce');
+
+    await page.getByRole('button', {name:'Login'}).click();
+
+    await expect(
+        page.getByText("Username and password do not match any user in this service")
+    ).toBeVisible();
+
+});
