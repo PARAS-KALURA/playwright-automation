47506513082c9baebbacb5088afea49230f6aaa3
Paras Kalura <officialkalura@gmail.com>
2026-09-24 07:30:10 +0530

    added Valid Login

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -1,12 +1,24 @@
 import { test, expect } from '@playwright/test';
 
+test("Valid Login", async ({page})  => {
+
+    await page.goto("https://www.saucedemo.com/");
+     await page.getByPlaceholder('Username').fill('standard_user');
+     await page.getByPlaceholder('Password').fill('secret_sauce');
+     await page.getByRole('button', {name:'Login'}).click();
+
+  await expect(page).toHaveURL(/inventory/);
+});
+
 test('Invalid Password', async ({page}) => {
  await page.goto('https://www.saucedemo.com/');
  await page.getByPlaceholder('Username').fill("standard_user");
  await page.getByPlaceholder('Password').fill("wrong_password");
  await page.getByRole('button', {name : "Login"}).click();
  
- await expect(page.getByText("Password do not match")).toBeVisible();
+await expect(
+  page.getByText("Username and password do not match any user in this service")
+).toBeVisible();
 
 })
 
@@ -24,3 +36,5 @@ test('Invalid Username', async ({page}) => {
     ).toBeVisible();
 
 });
+
+
